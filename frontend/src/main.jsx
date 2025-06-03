8c0161feaf8ba5d01157651a0c95ec037b29f26b
Ace-Chrono <richardszhao@gmail.com>
2025-06-03 18:45:43 -0400

    Made backend folder fully self contained

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,18 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom"
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider>
-        <App />      
-      </Provider>
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <BrowserRouter>
+        <Provider>
+          <App />      
+        </Provider>
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>,
 )
