39fd3a3aad8671c853d734a36cf45d369e347ebc
Ace-Chrono <richardszhao@gmail.com>
2025-06-03 20:09:47 -0400

    Clerk account management

--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { Container, VStack, Text, Button } from '@chakra-ui/react';
 
 const AccountPage = () => {
   return (
-    <div>AccountPage</div>
-  )
-}
+    <Container py={8}>
+      <VStack spacing={4}>
+        <SignedOut>
+          <Text fontSize="xl">You are not signed in.</Text>
+          <SignInButton mode="modal">
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
 
-export default AccountPage
+        <SignedIn>
+          <Text fontSize="xl">Welcome back!</Text>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+      </VStack>
+    </Container>
+  );
+};
+
+export default AccountPage;
