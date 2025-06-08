79c2e1f3e3477a0ebbf580e918a9d021329bbf92
Ace-Chrono <richardszhao@gmail.com>
2025-06-08 15:33:59 -0400

    account page updates

--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
-import { VStack, Container, Heading, Box, Button, Text, SimpleGrid } from '@chakra-ui/react';
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react';
+import { VStack, Container, Heading, Box, Button, Text, SimpleGrid, HStack } from '@chakra-ui/react';
 import { useUser } from "@clerk/clerk-react";
 import { useUserPresetStore } from '@/store/userPreset';
 import UserPresetCard from '@/components/UserPresetCard';
@@ -14,49 +14,64 @@ const AccountPage = () => {
   console.log("User Presets", userPresets)
 
   return (
-    <Container py={8}>
-      <VStack spacing={4}>
+    <Container py={8} maxW="container.lg">
+      <VStack spacing={4} align="start">
         <SignedOut>
           <Text fontSize="xl">You are not signed in.</Text>
-          <SignInButton mode="modal">
+          <SignInButton mode="modal" mt = {4}>
             <Button>Sign In</Button>
           </SignInButton>
         </SignedOut>
 
         <SignedIn>
-          <UserButton afterSignOutUrl="/" />
-          <Heading as = {"h1"} size = {"2xl"} textAlign = {"center"}>
-            Welcome, {user.firstName}
-          </Heading>
-          <Heading as = {"h1"} size = {"1xl"} textAlign = {"center"} mb = {8} mt={8}>
-            Presets
-          </Heading>
-          <Box maxH="640px" overflowY="auto" p={2} border="1px solid" borderColor="gray.700" borderRadius="md" mb = {8}>
-            <SimpleGrid
-                columns = {{
-                base: 1,
-                md: 3,
-                lg: 4
-                }}
-                spacing = {10}
-                w = {"full"}
-            >
-                {userPresets.map((userPreset) => (
-                <UserPresetCard key = {userPreset._id} userPreset = {userPreset} />
-                ))}
-            </SimpleGrid>
-            {userPresets.length === 0 && (
-                <Text
-                    fontSize = 'xl'
-                    textAlign = {"center"}
-                    fontWeight = {"bold"}
-                    color = 'gray.500'
-                >
-                    No user presets found
+          {user && (
+            <>
+              <HStack>
+              <UserButton afterSignOutUrl="/" />
+              <Heading as = {"h1"} size = {"3xl"}>
+                Welcome, {user.firstName}
+              </Heading>
+              </HStack>
+              <Box mt={4} textAlign="left">
+                <Text fontSize="lg">Username: {user.username || "Not set"}</Text>
+                <Text fontSize="lg">Email: {user.emailAddresses[0]?.emailAddress}</Text>
+                <Text fontSize="lg">
+                  Date Joined: {new Date(user.createdAt).toLocaleDateString()}
                 </Text>
-            )}
-          </Box>
-          <Button onClick={() => user.signOut()}>Sign Out</Button>
+                <Text fontSize="lg">User ID: {user.id}</Text>
+              </Box>
+              <Heading as = {"h1"} size = {"2xl"} textAlign = {"center"} mb = {4} mt={4}>
+                Presets
+              </Heading>
+              <Box maxH="640px" overflowY="auto" p={2} border="1px solid" borderColor="gray.700" borderRadius="md" mb = {8}>
+                <SimpleGrid
+                  columns = {{
+                  base: 1,
+                  md: 3,
+                  lg: 4
+                  }}
+                  spacing = {10}
+                  w = {"full"}
+                >
+                  {userPresets.map((userPreset) => (
+                    <UserPresetCard key = {userPreset._id} userPreset = {userPreset} />
+                  ))}
+                </SimpleGrid>
+                {userPresets.length === 0 && (
+                  <Text
+                      fontSize = 'xl'
+                      textAlign = {"center"}
+                      fontWeight = {"bold"}
+                      color = 'gray.500'
+                  >
+                    No user presets found
+                  </Text>
+                )}
+              </Box>
+              <SignOutButton redirectUrl="/" />
+            </>
+          )}
+          
         </SignedIn>
       </VStack>
     </Container>
