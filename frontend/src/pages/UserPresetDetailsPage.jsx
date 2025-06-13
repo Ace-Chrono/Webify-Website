26a21c47b2c3031889b0a7be20c4fae2e0eec66e
Ace-Chrono <richardszhao@gmail.com>
2025-06-13 09:30:18 -0400

    preset editing

--- frontend/src/pages/UserPresetDetailsPage.jsx
+++ frontend/src/pages/UserPresetDetailsPage.jsx
@@ -1,4 +1,5 @@
-import { Box, Text, Stack, Heading, Button, HStack, Spacer, Container } from '@chakra-ui/react';
+import { Box, Text, Stack, Heading, Button, HStack, Spacer, Container, Editable, IconButton, FileUpload } from '@chakra-ui/react';
+import { LuCheck, LuPencilLine, LuX } from "react-icons/lu"
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -81,24 +82,130 @@ const UserPresetDetails = () => {
     navigate('/');
   }
 
+  const { updateUserPreset } = useUserPresetStore();
+  const handleUpdate = async (newName, newSettings, newImage) => {
+    const token = await getToken();
+
+    let name = newName;
+    if (!name) {
+      name = preset.name;
+    }
+
+    let settingsFile = newSettings;
+    if (!settingsFile) {
+      const settingsBlob = new Blob(
+        [JSON.stringify(preset.settings)],
+        { type: 'application/json' }
+      );
+      settingsFile = new File([settingsBlob], "settings.json", {
+        type: 'application/json',
+      });
+    }
+
+    let imageFile = newImage;
+    if (!imageFile) {
+      const res = await fetch(preset.image); // This fetches the base64 image
+      const imageBlob = await res.blob();
+      imageFile = new File([imageBlob], "image.png", {
+          type: imageBlob.type || 'image/png'
+      });
+    }
+
+    const {success, message} = await updateUserPreset({
+      _id: preset._id,
+      name: name,
+      settings: settingsFile,
+      image: imageFile,
+      isPublished: true,
+      sourcePresetId: preset._id
+    }, token);
+
+    if (!success) {
+      toaster.create({
+        title: "Error",
+        description: message || "Failed to update user preset",
+        type: "error"
+      });
+      return;
+    }
+
+    toaster.create({
+      title: "Success",
+      description: "Preset updated successfully.",
+      type: "success"
+    });
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  }
+
   if (error) return <Text color="red.500">{error}</Text>;
   if (!preset) return <Text>Loading...</Text>;
 
   return (
     <Container>
-      <Heading size = {'3xl'} mb = {8} mt={8}> {preset.name}</Heading>
-      <img
+      <Editable.Root 
+        defaultValue={preset.name} mb = {8} mt={8} 
+        onValueCommit={(event) => {
+          const newName = event.value;
+          if (newName != preset.name) {
+            handleUpdate(newName, null, null);
+          }
+        }}>
+        <Editable.Preview fontSize="3xl"/>
+        <Editable.Input fontSize="3xl"/>
+        <Editable.Control style={{ marginTop: '8px' }}>
+          <Editable.EditTrigger asChild>
+            <IconButton variant="ghost" size="sm">
+              <LuPencilLine />
+            </IconButton>
+          </Editable.EditTrigger>
+          <Editable.CancelTrigger asChild>
+            <IconButton variant="outline" size="sm">
+              <LuX />
+            </IconButton>
+          </Editable.CancelTrigger>
+          <Editable.SubmitTrigger asChild>
+            <IconButton variant="outline" size="sm">
+              <LuCheck />
+            </IconButton>
+          </Editable.SubmitTrigger>
+        </Editable.Control>
+      </Editable.Root>
+      <div style={{ position: 'relative', width: '100%', aspectRatio: '16 / 9', margin: '0 auto' }}>
+        <img
           src={preset.image}
           alt={preset.name}
           style={{
-            width: '100%',               // Not full width, but fills most of the page
-            aspectRatio: '16 / 9',      // Keeps 1920×1080 ratio
+            width: '100%',
+            height: '100%',
             objectFit: 'cover',
             borderRadius: '8px',
-            display: 'block',           // Needed to center with margin
-            margin: '0 auto'            // Centers the image horizontally
+            display: 'block',
           }}
-      />
+        />
+
+        <Box position="absolute" top="8px" right="8px" zIndex="1">
+          <FileUpload.Root 
+            accept={["image/*"]}
+            onFileChange= {(details) => {
+              const files = details.acceptedFiles;
+              if (files.length > 0) {
+                handleUpdate(null, null, files[0]);
+              }
+            }}
+          >
+            <FileUpload.HiddenInput />
+              <FileUpload.Trigger asChild>
+                <Button variant="outline" size="xl" bg = "gray.900">
+                  <LuPencilLine />
+                </Button>
+              </FileUpload.Trigger>
+            <FileUpload.List />
+          </FileUpload.Root>
+        </Box>
+      </div>
 
       <HStack>
         <Link to="/">
@@ -108,12 +215,6 @@ const UserPresetDetails = () => {
         </Link>
 
         <Spacer />
-
-        <Link to="/">
-            <Button mt={4}>
-            Edit
-          </Button>
-        </Link>
 
         <Button mt={4} onClick={handleDelete}>
           Delete
@@ -140,17 +241,38 @@ const UserPresetDetails = () => {
       </Box>
 
       <Text fontSize="xl" fontWeight="bold" mt = {8} mb={4}>Settings:</Text>
-      <Box
-        p={4}
-        bg="gray.900"
-        color="white"
-        borderRadius="md"
-        overflowX="auto"
-        whiteSpace="pre-wrap"
-        width="100%"
-        mb = {8}
-      >
-        {JSON.stringify(preset.settings, null, 2)}
+      <Box position="relative" width="100%" mb={8}>
+        <Box position="absolute" top="8px" right="8px" zIndex="1">
+          <FileUpload.Root 
+            accept={[".json"]}
+            onFileChange= {(details) => {
+              const files = details.acceptedFiles;
+              if (files.length > 0) {
+                handleUpdate(null, files[0], null);
+              }
+            }}
+          >
+            <FileUpload.HiddenInput />
+              <FileUpload.Trigger asChild>
+                <Button variant="outline" size="xl" bg = "gray.900">
+                  <LuPencilLine />
+                </Button>
+              </FileUpload.Trigger>
+            <FileUpload.List />
+          </FileUpload.Root>
+        </Box>
+
+        <Box
+          p={4}
+          bg="gray.900"
+          color="white"
+          borderRadius="md"
+          overflowX="auto"
+          whiteSpace="pre-wrap"
+          width="100%"
+        >
+          {JSON.stringify(preset.settings, null, 2)}
+        </Box>
       </Box>
     </Container>
   );
